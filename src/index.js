342bceb4245816f92654af3d2d3a212d9086ac4f
apribooting <roflanPushka@gmail.com>
2023-02-21 16:38:14 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let m = String(n);
+    if (m[0] === "-") {
+        a = m.substring(1);
+        var splitArray = a.split("");
+        var reverseArray = splitArray.reverse("");
+        var end = reverseArray.join("");
+        return end;  
+    }
+    else {
+    
+        var splitArray = m.split("");
+        var reverseArray = splitArray.reverse("");
+        var end = reverseArray.join("");
+        return end; 
+    }
 }
